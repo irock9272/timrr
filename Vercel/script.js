c1369709fdbe8756a6c4c74dfecbcb548bfd44cb
irock9272 <128989044+irock9272@users.noreply.github.com>
2025-04-17 11:53:59 -0400

    removed vercel folder + updated readme

--- Vercel/script.js
+++
@@ -1,8 +0,0 @@
-function updateTime() {
-    const now = new Date();
-    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
-    document.getElementById('time').textContent = now.toLocaleTimeString('en-US', options).replace('AM', 'AM').replace('PM', 'PM');
-}
-
-setInterval(updateTime, 1000);
-window.onload = updateTime;
